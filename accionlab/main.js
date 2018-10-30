e45c5614fd5c92ac2485afb85711bd05a8e5f1bd
Luqman Paravetty <luckieee@gmail.com>
2018-10-31 00:57:23 +0530

    removed cors as it is not fixable

--- accionlab/main.js
+++ accionlab/main.js
@@ -1,24 +1,10 @@
 (function(angular) {
 	'use strict';
 	var app = angular.module('accionlab', []);
-	
-	app.config(function($httpProvider) {
-	  $httpProvider.defaults.useXDomain = true;
-	  delete $httpProvider.defaults.headers.common['X-Requested-With'];
-	});
-	
 	app.factory('mymethod', function($http){
-		delete $http.defaults.headers.common['X-Requested-With'];
 		return {
 			addlist: function(callback){
-				
-				
-				return $http({
-					method: 'GET',
-					url: 'http://jsonplaceholder.typicode.com/posts',
-					dataType: 'jsonp',
-					headers: {'Authorization': 'Token token=xxxxYYYYZzzz'}
-				}).then(callback);
+				$http.get('http://jsonplaceholder.typicode.com/posts').then(callback);
 			}
 		};
 	});
