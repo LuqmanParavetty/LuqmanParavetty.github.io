20bede11ba7a1246c8e4a07bac470cee9e74b3a6
Luqman Paravetty <luckieee@gmail.com>
2018-11-08 08:17:05 +0530

    added spec file with unit test case called with controller

--- accionlab/main_spec.js
+++ accionlab/main_spec.js
@@ -1,72 +1,48 @@
-var idClick = element(by.partialButtonText('ID'));
-var uidClick = element(by.partialButtonText('User ID'));
-var titleClick = element(by.partialButtonText('Title'));
-var bodyClick = element(by.partialButtonText('body'));
 
+describe("Unit Test Suite for accion lab ascending descending order table", function () {
+	var $controller, $rootScope, myMethod, $httpBackend;
+	beforeEach(module('accionlab'));
 
-var id = element(by.repeater('datas').column('data.id').row(0));
-var idLast = element(by.repeater('datas').column('data.id').row(99));
+	beforeEach(inject(function (_$controller_, _$rootScope_, _mymethod_, _$httpBackend_) {
+		$controller = _$controller_;
+		$rootScope = _$rootScope_;
+		myMethod = _mymethod_;
+		$httpBackend = _$httpBackend_;
+	}));
 
-var userid = element(by.repeater('datas').column('data.userId').row(0));
-var useridLast = element(by.repeater('datas').column('data.userId').row(99));
+	describe('Define sorting', function () {
+		it('It should have a method defined sorting to sort the posts based on type and order', function () {
+			var $scope = $rootScope.$new();
+			var controller = $controller('jschallengeController', { $scope: $scope });
+			expect($scope.sorting).toBeDefined();
+		});
 
-var title = element(by.repeater('datas').column('data.title').row(0));
-var titleLast = element(by.repeater('datas').column('data.title').row(99));
+		it('It should have a method sorting which set ', function () {
+			var $scope = $rootScope.$new();
+			var controller = $controller('jschallengeController', { $scope: $scope });
+			var sortBy = 'title';
+			$scope.directionFlag = false;
+			$scope.sorting(sortBy);
+			expect($scope.sorting).toBeDefined();
+			expect($scope.firstSelect).toBe(sortBy);
+			expect($scope.sortByWhat).toBe(sortBy);
+			expect($scope.directionFlag).toBeTruthy();
+		});
 
-var body = element(by.repeater('datas').column('data.body').row(0));
-var bodyLast = element(by.repeater('datas').column('data.body').row(99));
+		it('It should have a service injected myFactory', function () {
+			expect(myMethod).toBeDefined();
+			expect(myMethod.addlist).toBeDefined();
+			$httpBackend.expectGET('http://jsonplaceholder.typicode.com/posts').respond(angular.toJson(
+				[{ "userId": 1, "id": 1, "title": "Title", "body": "Test desc 1" },
+				{ "userId": 1, "id": 2, "title": "Title", "body": "Test desc 2" }]
+			));
+			myMethod.addlist(function (response) {
+				expect(response.data).toBeDefined();
+				expect(response.data.length).toEqual(2);
+			});
+			$httpBackend.flush();
+		})
 
+	})
 
-  describe("Unit Test Suite for accion lab ascending descending order table", function() {
-	it('should check srting in first row ID first colum ascending descending order', function() {
-	  expect(id.getText()).toBe('1');
-	  expect(idLast.getText()).toBe('100');
-
-	  idClick.click();
-	  expect(id.getText()).toBe('100');
-	  expect(idLast.getText()).toBe('1');
-
-	  idClick.click();
-	  expect(id.getText()).toBe('1');
-	  expect(idLast.getText()).toBe('100');
-	});
-
-	it('should check srting in second colum ascending descending order', function() {
-	  expect(userid.getText()).toBe('1');
-	  expect(useridLast.getText()).toBe('10');
-
-	  uidClick.click();
-	  expect(userid.getText()).toBe('10');
-	  expect(useridLast.getText()).toBe('1');
-
-	  uidClick.click();
-	  expect(userid.getText()).toBe('1');
-	  expect(useridLast.getText()).toBe('10');
-	});
-
-	it('should check srting in third colum ascending descending order', function() {
-	  expect(title.getText()).toContain('sunt aut facere');
-	  expect(titleLast.getText()).toBe('voluptatum itaque dolores ');
-
-	  titleClick.click();
-	  expect(title.getText()).toBe('voluptatum itaque dolores ');
-	  expect(titleLast.getText()).toContain('sunt aut facere');
-
-	  titleClick.click();
-	  expect(title.getText()).toContain('sunt aut facere');
-	  expect(titleLast.getText()).toBe('voluptatum itaque dolores ');
-	});
-
-	it('should check srting in fourth colum ascending descending order', function() {
-	  expect(body.getText()).toContain('quia et suscipit suscipit recusandae');
-	  expect(bodyLast.getText()).toContain('voluptatibus ex esse sint e');
-
-	  bodyClick.click();
-	  expect(body.getText()).toContain('voluptatibus ex esse sint e');
-	  expect(bodyLast.getText()).toContain('quia et suscipit suscipit recusandae');
-
-	  bodyClick.click();
-	  expect(body.getText()).toContain('quia et suscipit suscipit recusandae');
-	  expect(bodyLast.getText()).toContain('voluptatibus ex esse sint e');
-	});
-  });
+});
